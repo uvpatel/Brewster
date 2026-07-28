e93e5dd5505532e3a43ee886f203feeb4d571b06
Urvil Patel <uvpatel7271@gmail.com>
2026-07-28 22:31:55 +0530

    Fixed url

--- src/components/vortex-demo.tsx
+++ src/components/vortex-demo.tsx
@@ -4,26 +4,30 @@ import Link from "next/link";
 
 export default function VortexDemo() {
   return (
-    <div className="w-full mx-auto rounded-md  h-screen overflow-hidden">
+    <div className="w-full mx-auto rounded-md h-screen overflow-hidden">
       <Vortex
         backgroundColor="black"
         className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
       >
         <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-         Cafe
+          Cafe
         </h2>
         <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          Welcome to our cafe! We offer a cozy and inviting atmosphere where you can enjoy a variety of delicious beverages and treats. 
+          Welcome to our cafe! We offer a cozy and inviting atmosphere where you can enjoy a variety of delicious beverages and treats.
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-          <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
-            <Link href="/coupans"> Order Now </Link>
-          </button>
-          <button className="px-4 py-2  text-white "> 
-            <Link href="/dashboard">
-            Dashboard 
-            </Link>
-            </button>
+          <Link
+            href="/dashboard/pos"
+            className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset] inline-block font-medium"
+          >
+            Order Now
+          </Link>
+          <Link
+            href="/dashboard"
+            className="px-4 py-2 text-white hover:underline inline-block font-medium"
+          >
+            Dashboard
+          </Link>
         </div>
       </Vortex>
     </div>
